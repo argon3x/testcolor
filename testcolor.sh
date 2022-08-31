0e1b7254d6fc1d2ba6617bc0fa80f8bf6693e933
dobleemme <112032808+dobleemme@users.noreply.github.com>
2022-08-31 11:44:38 -0500

    Add files via upload

---
+++ testcolor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+echo -e "\n\t\e[00;34m >>> Azul Oscuro <<< \e[00m"
+echo -e "\t\e[01;34m >>> Azul  <<< \e[00m"
+echo -e "\t\e[00;32m >>> Verde Oscuro <<< \e[00m"
+echo -e "\t\e[01;32m >>> Verde  <<< \e[00m"
+echo -e "\t\e[00;36m >>> Cyan Oscuro <<< \e[00m"
+echo -e "\t\e[01;36m >>> Cyan  <<< \e[00m"
+echo -e "\t\e[00;31m >>> Rojo Oscuro <<< \e[00m"
+echo -e "\t\e[01;31m >>> Rojo  <<< \e[00m"
+echo -e "\t\e[00;35m >>> Morado Oscuro <<< \e[00m"
+echo -e "\t\e[01;35m >>> Morado  <<< \e[00m"
+echo -e "\t\e[00;33m >>> Amarillo Oscuro <<< \e[00m"
+echo -e "\t\e[01;33m >>> Amarillo  <<< \e[00m"
+echo -e "\t\e[00;37m >>> Gris <<< \e[00m"
+echo -e "\t\e[01;37m >>> Blanco <<< \e[00m\n"
